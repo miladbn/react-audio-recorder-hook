6060493d9982153a51a2e74b888d8d841c20b000
miladbn <milad.b.n.77@gmail.com>
2025-03-14 20:34:21 +0330

    chore: Update pnpm-lock.yaml with new devDependencies and their versions; ensure consistent formatting in index.ts export statement

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,1 +1,1 @@
-export { default as AudioRecorderWithVisualization } from './AudioRecorderWithVisualization'; 
+export { default as AudioRecorderWithVisualization } from './AudioRecorderWithVisualization';
